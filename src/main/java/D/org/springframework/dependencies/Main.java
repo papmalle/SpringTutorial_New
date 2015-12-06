fd1ead48bdf9c8af4f10bdf006de04cbfc7b76a5
root <root@malle-N76VB>
2015-12-06 20:45:00 +0100

    Mise en forme

--- src/main/java/D/org/springframework/dependencies/Main.java
+++ src/main/java/D/org/springframework/dependencies/Main.java
@@ -9,27 +9,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		context = new ClassPathXmlApplicationContext("D/datasource.xml");
-		//LoolClass loolClass =context.getBean("Lool", LoolClass.class);
-		//System.out.println(loolClass.getProperties().getProperty("jdbc.driver.className"));
-		
-	BasicDataSource dataSource = context.getBean("myDataSource", BasicDataSource.class);
-	System.out.println("----------------------ddsss--------------1");
-	System.out.println(dataSource.getUsername());
-dataSource.setUsername("yaya");
-System.out.println(dataSource.getUsername());
-BasicDataSource dataSource1 = context.getBean("myDataSource", BasicDataSource.class);
-System.out.println("--------------ff----------------------2");
-System.out.println(dataSource1.getUsername());
-dataSource.setUsername("yayssa toooo");
-System.out.println(dataSource1.getUsername());
-System.out.println("------------------------------fghjh------1");
-System.out.println(dataSource.getUsername());
-dataSource.setUsername("yayxxxa");
-System.out.println("dddddd"+dataSource.getUsername());
-//		LoolClass loolClass = dataSource.getLool();
-//			
-//		System.out.println(dataSource.toString());
-//		System.out.println(loolClass.getProperties().getProperty("jdbc.driver.className"));
+		// LoolClass loolClass =context.getBean("Lool", LoolClass.class);
+		// System.out.println(loolClass.getProperties().getProperty("jdbc.driver.className"));
+
+		BasicDataSource dataSource = context.getBean("myDataSource", BasicDataSource.class);
+		System.out.println("----------------------ddsss--------------1");
+		System.out.println(dataSource.getUsername());
+		dataSource.setUsername("yaya");
+		System.out.println(dataSource.getUsername());
+		BasicDataSource dataSource1 = context.getBean("myDataSource", BasicDataSource.class);
+		System.out.println("--------------ff----------------------2");
+		System.out.println(dataSource1.getUsername());
+		dataSource.setUsername("yayssa toooo");
+		System.out.println(dataSource1.getUsername());
+		System.out.println("------------------------------fghjh------1");
+		System.out.println(dataSource.getUsername());
+		dataSource.setUsername("yayxxxa");
+		System.out.println("dddddd" + dataSource.getUsername());
+		// LoolClass loolClass = dataSource.getLool();
+		//
+		// System.out.println(dataSource.toString());
+		// System.out.println(loolClass.getProperties().getProperty("jdbc.driver.className"));
 
 	}
 
